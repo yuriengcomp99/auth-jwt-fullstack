240658cf7a8729b7b44384e8f7d6d79b19c3529e
yuriengcomp99 <yuri.engcomp99@gmail.com>
2026-03-27 15:22:27 -0300

    refactor(backend): fix TypeScript configuration and correct Express router setup

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,10 @@
 import { makeRegisterController } from "../factories/auth/make-register.js"
 import { makeLoginController } from "../factories/auth/make-login.js"
+import { Router } from "express"
 
 const registerController = makeRegisterController()
 const loginController = makeLoginController()
+const router = Router()
 
 router.post("/register", (req, res) => {
   return registerController.handle(req, res)
@@ -11,3 +13,5 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   return loginController.handle(req, res)
 })
+
+export default router
