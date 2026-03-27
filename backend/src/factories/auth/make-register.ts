5147691f6126a865044b6095921a2d0063d77717
yuriengcomp99 <yuri.engcomp99@gmail.com>
2026-03-27 15:12:47 -0300

    refactor(backend): create factory to assemble RegisterController with dependencies

--- backend/src/factories/auth/make-register.ts
+++ backend/src/factories/auth/make-register.ts
@@ -1,10 +1,13 @@
 import { UserRepository } from "../../repositories/user/user.repository.js"
 import { RegisterUseCase } from "../../use-cases/auth/register.usecase.js"
+import { RegisterController } from "../../controllers/auth/register.controller.js"
 
-export function makeRegisterUseCase() {
+export function makeRegisterController() {
   const userRepository = new UserRepository()
 
   const registerUseCase = new RegisterUseCase(userRepository)
 
-  return registerUseCase
+  const registerController = new RegisterController(registerUseCase)
+
+  return registerController
 }
