4de7c72a9c3bb9d93bddab5cdc0defdbc1ae03b2
yuriengcomp99 <yuri.engcomp99@gmail.com>
2026-03-27 14:55:47 -0300

    feat(backend): implement login use case with JWT authentication

---
+++ backend/src/use-cases/auth/login.usecase.ts
@@ -0,0 +1,39 @@
+import { UserRepository } from "../../repositories/user/user.repository.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+export class LoginUseCase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(data: { email: string; password: string }) {
+    const { email, password } = data
+
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error("Invalid credentials")
+    }
+
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      throw new Error("Invalid credentials")
+    }
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "15m" }
+    )
+
+    return {
+      accessToken: token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email
+      }
+    }
+  }
+}
